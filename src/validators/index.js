be4c6f611dad4c6a7dbf5c0a9ff8a405ccb45c13
Anshuman-Sahu-23-06 <anshumansahu2306@gmail.com>
2026-08-16 15:25:07 +0530

    Update

---
+++ src/validators/index.js
@@ -0,0 +1,36 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () =>{
+    return[
+        body("email")
+        .trim()
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail()
+        .withMessage("Email is not valid")
+        .normalizeEmail(),
+
+        body("username")
+        .trim()
+        .notEmpty()
+        .withMessage("Username is required")
+        .isLength({min: 3})
+        .withMessage("Username must be at least 3 characters long"),
+        
+        body("password")
+        .trim()
+        .notEmpty()
+        .withMessage("Password is required")
+        .isLength({min: 6})
+        .withMessage("Password must be at least 6 characters long"),
+        
+        body("fullname")
+        .trim()
+        .notEmpty()
+        .withMessage("Fullname is required")
+        .isLength({min: 3})
+        .withMessage("Fullname must be at least 3 characters long")
+    ]
+}
+
+export{userRegisterValidator}
