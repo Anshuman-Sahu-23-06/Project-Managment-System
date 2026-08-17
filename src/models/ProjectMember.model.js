2ad82320d21e2b1835519cf93269c3e3ca1388d9
Anshuman-Sahu-23-06 <anshumansahu2306@gmail.com>
2026-08-17 12:19:22 +0530

    Update

---
+++ src/models/ProjectMember.model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Types } from "mongoose";
+import { AvailableUserRole, UserRoleEnum} from "../utils/constants.js";
+
+const projectMemberSchema = new Schema({
+
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    project: {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+        required: true
+    },
+    role: {
+        type: String,
+        enum: AvailableUserRole,
+        default: UserRoleEnum.MEMBER
+    }
+},
+
+{timestamps: true})
+
+export const Project = mongoose.model("Project",ProjectSchema)
